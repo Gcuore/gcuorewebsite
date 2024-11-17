49554b128a048949753bd6383748fdea9a7c5db5
Gcuore <165587875+Gcuore@users.noreply.github.com>
2024-11-17 19:39:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-// Aggiunge un evento click al bottone per aprire il link
-document.getElementById('linkButton').addEventListener('click', function () {
+// Bottone per YouTube
+document.getElementById('youtubeButton').addEventListener('click', function () {
     window.open('https://www.youtube.com/channel/UCS7S9UYGn2nR8SZSloBYTcg', '_blank');
+});
+
+// Bottone per GitHub
+document.getElementById('githubButton').addEventListener('click', function () {
+    window.open('https://github.com/Gcuore', '_blank');
 });
